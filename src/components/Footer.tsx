e74c8eed7edd8c3e77985ab70ccf75030e38597a
omarghandour <omarkhaled9913@gmail.com>
2024-10-26 16:11:31 +0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -31,7 +31,7 @@ export const Footer = () => {
           <Text paddingX="4">{person.name}</Text>
           <Text onBackground="neutral-weak">
             {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-            / Build your portfolio with{" "}
+            / Build your Web/Mobile/Desktop Apps with{" "}
             <SmartLink style={{ marginLeft: "-0.125rem" }} href="/about">
               Black Waves
             </SmartLink>
